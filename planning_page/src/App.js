0a5565bb54e52c043a0a83b6b7b4502249187ea7
Deye Lei <deye.Lei76@myhunter.cuny.edu>
2021-06-29 15:24:17 -0400

    New functions added, code restructured

--- planning_page/src/App.js
+++ planning_page/src/App.js
@@ -1,17 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React from 'react';
 
 function Navigation_bar(Pros){
   return (
     <ul id = "navigation_bar">
-			<li class= "navigation_item">	
+			<li className= "navigation_item">	
 				<a href="">Home</a>
 			</li>
-			<li class= "navigation_item">
+			<li className= "navigation_item">
 				<a href="">Service information</a>
 			</li>
-			<li class= "navigation_item">
+			<li className= "navigation_item">
 				<a href="">Contact us</a>
 			</li>
 		</ul>
@@ -111,30 +111,123 @@ class SavingList extends React.Component{
   }
 }
 
-class AddInputBox extends React.Component{
+class Planning_Componenet extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      wantList_Items:[],
+      needList_Items:[],
+      input_Name : "",
+      input_Price : "",
+      Wantchecked: false, //zero for Need
+      Needchecked: false
+    };
+  }
+  
+  selectWant = (e) =>{
+    var value = e.target.checked;
+    this.setState({Wantchecked: value, Needchecked:!value})
+
+  }
+
+  selectNeed = (e) =>{
+    var value = e.target.checked;
+    this.setState({Needchecked: value, Wantchecked:!value})
+  }
+
+  handleNameInput = (e)=>{
+    var name = e.target.value;
+    this.setState({input_Name: name})
+  }
+
+  handlePriceInput = (e)=>{
+    var price = e.target.value;
+    this.setState({input_Price: price})
+  }
+
+  add = () =>{
+    var newObject = {Name: this.state.input_Name, Price: this.state.input_Price};
+    var copy_array = [];
+    if(this.state.Wantchecked)
+    {
+      copy_array = [...this.state.wantList_Items, newObject];
+      this.setState({wantList_Items: copy_array})
+    }
+    else{
+      copy_array = [...this.state.needList_Items, newObject];
+      this.setState({needList_Items: copy_array})
+    }
+    //this.resetInput();uncommon this for user
+    
+  }
+
+
+  resetInput = () =>{
+    this.setState({
+      input_Name : "",
+      input_Price : "",
+      Wantchecked: false, 
+      Needchecked: false
+    })
+  }
+
   render(){
     return(
+      <div>
+      <div id="WantList_Container">
+      <h3>Wants List:(currently 30%) </h3>
+      <div id = "Want_Remaining_Budget">*Remaining Budget: $0</div>
+      <ul  id = "Want_List">
+        {this.state.wantList_Items.map(
+          (Items)=> <li key = {Items.Name}><input type = "checkBox"/>{Items.Name}: ${Items.Price} </li>
+        )}
+      </ul>
+      <button onClick = {this.delete_item} name= "delete_button" type = "botton" id = "WantList_Delete">Delete</button>
+
+      <button onClick = {this.reset} name= "reset_button" type = "reset" id = "WantList_Reset">Reset</button>
+      </div>
+
+
+      <div id="NeedList_Container">
+			<h3>Needs List:(currently 50%) </h3>
+			<div id = "Need_Remaining_Budget">*Remaining Budget: $0</div>
+			<ul id = "Need_List">
+       {this.state.needList_Items.map(
+          (Items)=> <li key = {Items.Name}><input type = "checkBox"/>{Items.Name}: ${Items.Price} </li>
+        )}
+			</ul>
+			
+				
+			<button name= "delete_button" id = "NeedList_Delete">Delete</button>
+
+			<button name= "reset_button" type = "reset" value = "Reset" id = "NeedList_Reset">Reset</button>
+		  </div>
+
+      <SavingList/>
+
+
       <div id = "addinput">
 			<h3>Add your item</h3>
 			
-			<label for = "Item Type" id = "ItemType_Label">Item type:</label>
-			<label for ="want">Want</label>
-			<input type= "radio" id = "WantRadio" name = "Item Type"/>
+			<label htmlFor = "Item Type" id = "ItemType_Label">Item type:</label>
+			<label htmlFor ="want">Want</label>
+			<input type= "radio" id = "WantRadio" checked = {this.state.Wantchecked} onChange = {this.selectWant}/>
 
-			<label for ="Need">Need</label>
-			<input type= "radio" id = "NeedRadio" name = "Item Type"/>
+			<label htmlFor ="Need">Need</label>
+			<input type= "radio" id = "NeedRadio" name = "Item Type" checked = {this.state.Needchecked} onChange = {this.selectNeed}/>
 			<br/>
 
-			<label for = "NameTextBox" id = "NameTextBox_Label">Name: </label>
-			<input type = "text" id = "NameTextBox" placeholder = "Ex: Ps5"/>
+			<label htmlFor = "NameTextBox" id = "NameTextBox_Label">Name: </label>
+			<input type = "text" id = "NameTextBox" placeholder = "Ex: Ps5" value = {this.state.input_Name} onChange = {this.handleNameInput}/>
 			<br/>
-			<label for = "PriceBox" id="PriceBox_Label">Price: </label>
-			<input type = "number" id = "PriceBox" placeholder= "Ex: 399"/>
+			<label htmlFor = "PriceBox" id="PriceBox_Label">Price: </label>
+			<input type = "number" id = "PriceBox" placeholder= "Ex: 399" value = {this.state.input_Price} onChange = {this.handlePriceInput}/>
 
-			<button class = "ADDInputButton" id = "AddInput_Add_to_list">Add to list </button>
+			<button className = "ADDInputButton" id = "AddInput_Add_to_list" onClick = {this.add}>Add to list </button>
 
-			<button class = "ADDInputButton" id = "AddInput_Reset">Reset </button>
+			<button className = "ADDInputButton" id = "AddInput_Reset" onClick = {this.resetInput}>Reset </button>
   	  </div>
+      </div>
     );
   }
 }
@@ -143,10 +236,7 @@ function App() {
     <div>
     <Navigation_bar/>
     <Total_budgetBar/>
-    <WantList/>
-    <NeedList/>
-    <SavingList/>
-    <AddInputBox/>
+    <Planning_Componenet/>
     </div>
   );
 }

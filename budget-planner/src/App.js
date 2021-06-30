f23c86fb38ac06df9e5ad10f80876d49c06c728e
Deye Lei <deye.lei76@myhunter.cuny.edu>
2021-06-30 00:20:17 -0400

    someChanges

--- budget-planner/src/App.js
+++ budget-planner/src/App.js
@@ -1,14 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import Planning_Componenet from './components/Planning_Component';
+import Planning_Component from './components/Planning_Component';
 
 class App extends React.Component{
+  constructor(Props){
+    super(Props);
+    this.state = {
+      display: true
+      
+    };
+
+  }
   render(){
+    let display_Component;
+    if(this.state.display == false)
+    {
+      //alex's component
+    }
+    else
+    {
+      display_Component = <Planning_Component/>;
+    }
     return (
         <div>
-        <h1>hello world</h1>
-        <Planning_Componenet/>
+        {display_Component}
         </div>
       )
   }

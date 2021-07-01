841bd1cb85442f8bed82aa2c231b57a169805639
Deye Lei <deye.lei76@myhunter.cuny.edu>
2021-07-01 00:33:20 -0400

    Combined pages

--- budget-planner/src/App.js
+++ budget-planner/src/App.js
@@ -10,6 +10,7 @@ class App extends React.Component{
   constructor(Props){
     super(Props);
     this.state = {
+      display:false,
       Items:[],
       wantList_Items:[],
       needList_Items:[],
@@ -17,11 +18,261 @@ class App extends React.Component{
       input_Price : "",
       Wantchecked: false, //zero for Need
       Needchecked: false,
-      Balance:0
+      Balance:0.00,
+      Need_Amount:0.00,
+      Want_Amount:0.00,
+      Saving_Amount:0.00,
+      Need_Percent:0,
+      Want_Percent:0,
+      Saving_Percent:0
 
     };
 
   }
+  /********************************** */
+  //Homepage functions
+  addamt = ()=>{
+    this.addfunction();
+    var value = document.getElementById("amt1").innerHTML;
+    this.setState({Balance:value})
+  }
+
+  minusamt = ()=>{
+    this.minusfunction();
+    var value = document.getElementById("amt1").innerHTML;
+    this.setState({Balance:value})
+  }
+
+  clearamt = ()=>{
+    this.clearfunction();
+    var amt = document.getElementById("amt1").innerHTML;
+    var needamt = document.getElementById("needamt").innerHTML;
+    var wantamt = document.getElementById("wantamt").innerHTML;
+    var savingamt = document.getElementById("savingamt").innerHTML;
+    var needp = document.getElementById("need-percent").innerHTML;
+    var wantp = document.getElementById("want-percent").innerHTML;
+    var savingp = document.getElementById("saving-percent").innerHTML
+    this.setState({Balance:amt})
+    this.setState({Need_Amount:needamt})
+    this.setState({Want_Amount:wantamt})
+    this.setState({Saving_Amount:savingamt})
+    this.setState({Need_Percent:needp})
+    this.setState({Want_Percent:wantp})
+    this.setState({Saving_Percent:savingp})
+  }
+
+  needdelete= ()=>{
+    this.needresetfunction();
+    var needamt = document.getElementById("needamt").innerHTML;
+    var needp = document.getElementById("need-percent").innerHTML;
+    this.setState({Need_Amount:needamt})
+    this.setState({Need_Percent:needp})
+  }
+
+  needenter= ()=>{
+    this.needsfunction();
+    var needamt = document.getElementById("needamt").innerHTML;
+    var needp = document.getElementById("need-percent").innerHTML;
+    this.setState({Need_Amount:needamt})
+    this.setState({Need_Percent:needp})
+  }
+
+  wantdelete= ()=>{
+    this.wantresetfunction();
+    var wantamt = document.getElementById("wantamt").innerHTML;
+    var wantp = document.getElementById("want-percent").innerHTML;
+    this.setState({Want_Amount:wantamt})
+    this.setState({Want_Percent:wantp})
+  }
+
+  wantenter= ()=>{
+    this.wantsfunction();
+    var wantamt = document.getElementById("wantamt").innerHTML;
+    var wantp = document.getElementById("want-percent").innerHTML;
+    this.setState({Want_Amount:wantamt})
+    this.setState({Want_Percent:wantp})
+  }
+
+  savingdelete= ()=>{
+    this.savingresetfunction();
+    var savingamt = document.getElementById("savingamt").innerHTML;
+    var savingp = document.getElementById("saving-percent").innerHTML;
+    this.setState({Saving_Amount:savingamt})
+    this.setState({Saving_Percent:savingp})
+  }
+
+  savingenter= ()=>{
+    this.savingsfunction();
+    var savingamt = document.getElementById("savingamt").innerHTML;
+    var savingp = document.getElementById("saving-percent").innerHTML;
+    this.setState({Saving_Amount:savingamt})
+    this.setState({Saving_Percent:savingp})
+  }
+
+  changePage = ()=>{
+    this.planfunction();
+    var savingamt = document.getElementById("savingamt").innerHTML;
+    var savingp = document.getElementById("saving-percent").innerHTML;
+    this.setState({Need_Amount:savingamt})
+    this.setState({Need_Percent:savingp})
+    var value = !this.state.display;
+    this.setState({display:value})
+  }
+
+  addfunction(){
+    let y = document.getElementById("amt1").innerHTML;    /*y is a budget balance value */
+    let x = document.getElementById("input1").value;       /*x is a input value */
+    if(x<0 ||x==""){ //check for negative input or empty input
+        alert("the value cannot be negative or the input cannot be empty")
+    }
+    else{
+    let z = (x-0)+(y-0); // z = input value + budget balance value
+    document.getElementById("amt1").innerHTML=z.toFixed(2); //set budget balance value with z
+    document.getElementById("input1").value=""; //then reset every other inputs and budget percentages to zero when you add a new value for budget balance
+    document.getElementById("needamt").innerHTML="0.00"
+    document.getElementById("wantamt").innerHTML="0.00"
+    document.getElementById("savingamt").innerHTML="0.00"
+    document.getElementById("need-percent").innerHTML="0"
+    document.getElementById("want-percent").innerHTML="0"
+    document.getElementById("saving-percent").innerHTML="0"
+    document.getElementById("counter").innerHTML="100"
+                            //the same comments apply to minusfunction 
+    }
+}
+minusfunction(){
+    let y = document.getElementById("amt1").innerHTML;
+    let x = document.getElementById("input1").value;
+    if(x<0 ||x==""){
+        alert("the value cannot be negative or the input cannot be empty")
+    }else{
+    if(y-x<0){
+        alert("The input budget exceeded the current budget")
+    }else{
+    let z = y-x;
+    document.getElementById("amt1").innerHTML=z.toFixed(2);
+    document.getElementById("input1").value="";
+    document.getElementById("needamt").innerHTML="0.00"
+    document.getElementById("wantamt").innerHTML="0.00"
+    document.getElementById("savingamt").innerHTML="0.00"
+    document.getElementById("need-percent").innerHTML="0"
+    document.getElementById("want-percent").innerHTML="0"
+    document.getElementById("saving-percent").innerHTML="0"
+    document.getElementById("counter").innerHTML="100"
+    }
+}
+}
+clearfunction(){
+    //the clear function basically set all other value to zero
+    document.getElementById("amt1").innerHTML="0.00"
+    document.getElementById("needamt").innerHTML="0.00"
+    document.getElementById("wantamt").innerHTML="0.00"
+    document.getElementById("savingamt").innerHTML="0.00"
+    document.getElementById("need-percent").innerHTML="0"
+    document.getElementById("want-percent").innerHTML="0"
+    document.getElementById("saving-percent").innerHTML="0"
+    document.getElementById("counter").innerHTML="100"
+}
+
+needsfunction(){
+    let x = document.getElementById("amt1").innerHTML;    /*x is Budget balance value*/
+    let n = document.getElementById("needinput").value; /*n is need box input value*/
+    let m=document.getElementById("counter").innerHTML; /*m is a counter for budget left value*/
+    let y = document.getElementById("need-percent").innerHTML; /*y is need percentage value*/
+    if(n<0 || x==0 || n==""){   /*check for if the input value is negative or budget balance is zero or need box input is empty */
+        alert("the input cannot be negative or the input cannot be empty")
+    }else if((m-n)<0 &&(((m-0)+(y-0))-n)<0){ /*check if counter value minus need box input value is less than zero or counter value add need percentage then minus need input value is less than zero*/
+        alert("the budget percent input is too big");
+    }
+    else{
+        m=(m-0)+(y-0);/*  set m = counter value add need percentage value  */
+        m=m-n; /*set m = counter value minus need box input value*/
+    document.getElementById("counter").innerHTML=m.toFixed(2); /*set counter value = new counter value with 2 decemal places */
+    document.getElementById("needamt").innerHTML=(x*(n/100)).toFixed(2);/*set need amount = budget balance value*(need input /100) */
+    document.getElementById("need-percent").innerHTML=(n-0)-0;/*set need percentage with new need box input value  */
+                                            /*the same comments above apply to the two functions below */
+    }
+} 
+
+wantsfunction(){
+    let x = document.getElementById("amt1").innerHTML;
+    let n = document.getElementById("wantinput").value;
+    let m=document.getElementById("counter").innerHTML;
+    let y = document.getElementById("want-percent").innerHTML;
+    if(n<0 || x==0 || n==""){   /*check for negative input*/
+        alert("the input cannot be negative or the input cannot be empty")
+    }else if((m-n)<0 &&(((m-0)+(y-0))-n)<0){
+        alert("the budget percent input is too big");
+    }
+    else{
+        m=(m-0)+(y-0)
+        m=m-n;
+    document.getElementById("counter").innerHTML=m.toFixed(2);
+    document.getElementById("wantamt").innerHTML=(x*(n/100)).toFixed(2);
+    document.getElementById("want-percent").innerHTML=(n-0)-0;
+
+    }
+} 
+savingsfunction(){
+    let x = document.getElementById("amt1").innerHTML;
+    let n = document.getElementById("savinginput").value;
+    let m=document.getElementById("counter").innerHTML;
+    let y = document.getElementById("saving-percent").innerHTML;
+    if(n<0 || x==0 ||n==""){   /*check for negative input*/
+        alert("the input cannot be negative or the input cannot be empty")
+    }else if((m-n)<0 &&(((m-0)+(y-0))-n)<0){
+        alert("the budget percent input is too big");
+    }
+    else{
+        m=(m-0)+(y-0)
+        m=m-n;
+    document.getElementById("counter").innerHTML=m.toFixed(2);
+    document.getElementById("savingamt").innerHTML=(x*(n/100)).toFixed(2);
+    document.getElementById("saving-percent").innerHTML=(n-0)-0;
+
+    }
+}
+
+    needresetfunction(){
+        let x = document.getElementById("need-percent").innerHTML;
+        let y = document.getElementById("counter").innerHTML;
+        document.getElementById("need-percent").innerHTML=0;
+        document.getElementById("needamt").innerHTML=(0).toFixed(2);
+        document.getElementById("needinput").value="";
+        document.getElementById("counter").innerHTML=((x-0)+(y-0)).toFixed(2);
+    } 
+    wantresetfunction(){
+        let x = document.getElementById("want-percent").innerHTML;
+        let y = document.getElementById("counter").innerHTML;
+        document.getElementById("want-percent").innerHTML=0;
+        document.getElementById("wantamt").innerHTML=(0).toFixed(2);
+        document.getElementById("wantinput").value="";
+        document.getElementById("counter").innerHTML=((x-0)+(y-0)).toFixed(2);
+    }
+    savingresetfunction(){
+        let x = document.getElementById("saving-percent").innerHTML;
+        let y = document.getElementById("counter").innerHTML;
+        document.getElementById("saving-percent").innerHTML=0;
+        document.getElementById("savingamt").innerHTML=(0).toFixed(2);
+        document.getElementById("savinginput").value="";
+        document.getElementById("counter").innerHTML=((x-0)+(y-0)).toFixed(2);
+    }
+
+    planfunction(){
+        let x= document.getElementById("counter").innerHTML;
+        let y= document.getElementById("saving-percent").innerHTML;
+        let m=document.getElementById("amt1").innerHTML;
+        if(x!=0){
+            let n = (x-0)+(y-0);
+            m=m*(n/100);
+            document.getElementById("saving-percent").innerHTML=n;
+            document.getElementById("savingamt").innerHTML=m.toFixed(2);
+            document.getElementById("counter").innerHTML=0;
+
+        }else{
+            alert("go to planning page");
+        }
+
+    }
 
   /**************************************************************** */
   //Planning page function
@@ -183,7 +434,44 @@ class App extends React.Component{
     var display_Component;
     if(this.state.display == false)
     {
-      display_Component = <HomePage/>//if part of the code do not need setState place it in a sepepated component
+      display_Component = display_Component =  <div>
+      <h1 >Budget Balance:$<span id="amt1">{this.state.Balance}</span></h1>
+      <button onClick={this.minusamt} id="minus1">-</button>
+      <input type="number" id="input1" placeholder="Enter Budget Amount" min="0"></input>
+      <button onClick={this.addamt} id="add1">+</button>
+      <br></br>
+      <button onClick={this.clearamt} id="clear1">Clear</button>
+      <br></br>
+      <p id="p1">Needs
+                  <br></br>
+                  <p>Budget:<span id="need-percent">{this.state.Need_Percent}</span>%</p>
+                  <p>Amount:$<span id="needamt">{this.state.Need_Amount}</span></p>
+                  <input id="needinput" type="number" placeholder="Budget Percentage"></input>%
+                  <br></br>
+                  <button onClick={this.needdelete} id="bt">Reset</button>
+                  <button onClick={this.needenter} id="bt">Enter</button>
+                  </p>
+      
+                  <p id="p1">Wants
+                  <br></br>
+                  <p>Budget:<span id="want-percent">{this.state.Want_Percent}</span>%</p>
+                  <p>Amount:$<span id="wantamt">{this.state.Want_Amount}</span></p>
+                  <input id="wantinput" type="number" placeholder="Budget Percentage"></input>%
+                  <button onClick={this.wantdelete} id="bt">Reset</button>
+                  <button onClick={this.wantenter} id="bt">Enter</button>
+                  </p>
+      
+                  <p id="p1">Savings
+                  <br></br>
+                  <p>Budget:<span id="saving-percent">{this.state.Saving_Percent}</span>%</p>
+                  <p>Amount:$<span id="savingamt">{this.state.Saving_Amount}</span></p>
+                  <input id="savinginput" type="number" placeholder="Budget Percentage"></input>%
+                  <button onClick={this.savingdelete} id="bt">Reset</button>
+                  <button onClick={this.savingenter} id="bt">Enter</button>
+                  </p>
+                  <h2>Budget Available:<span id="counter">0</span>%</h2>
+                  <button onClick={this.changePage} id="bt1">Go to Planning</button>
+      </div>;
     }
     else
     {
@@ -243,7 +531,6 @@ class App extends React.Component{
         <div className = "App">
         
         {display_Component}
-        <button onClick = {this.changePage}>Change</button>
         </div>
       )
   }

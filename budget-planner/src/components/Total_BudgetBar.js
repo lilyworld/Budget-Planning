37651ebafebe5fbde033de2155d220ddd7f45a53
Deye Lei <deye.lei76@myhunter.cuny.edu>
2021-07-01 22:55:16 -0400

    All button work, and data flow from different pages now

--- budget-planner/src/components/Total_BudgetBar.js
+++ budget-planner/src/components/Total_BudgetBar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 function Total_budgetBar(Props){
-    return (<div><h1 id = "budget_bar">Total Budget: $3000 </h1></div>);
+    return (<div><h1 id = "budget_bar">Total Budget: ${Props.Total_Amount} </h1></div>);
   }
 
   export default Total_budgetBar

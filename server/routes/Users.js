2eabcff3dbc6923b51609cd4aea2e04805e42f17
Li Gao <ligao0807@gmail.com>
2021-07-22 22:04:49 -0400

    modified register databases

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const {Users} = require("../models");
+const bcrypt = require("bcrypt");
 
 router.get('/', async (req, res) => {
     const listOfUsers = await Users.findAll();
@@ -8,9 +9,25 @@ router.get('/', async (req, res) => {
 });
 
 router.post("/", async (req,res) => {
-    const user = req.body;
-    await Users.create(user);
-    res.json(user);
-})
+    const {username, email, password} = req.body;
+    bcrypt.hash(password, 10).then((hash) => {
+        Users.create({
+            username:username,
+            email:email,
+            password: hash,
+        })
+        res.json("SUCCESS");
+    })
+});
+
+router.post("/login", async(req, res) => {
+    const{email, password} = req.body;
+    const user = await Users.findOne({where : {email:email}});
+    if(!user) res.json({error: "User Doesn't Exist"});
+    bcrypt.compare(password, user.password).then((match) =>{
+        if (!match) res.json({error: "Wrong username and password combination"});
+        res.json("You Logged In");
+    });
+});
 
 module.exports = router;

42a7d07d53ef40b9e7607b0408fe18e21f77fed6
Deye Lei <deye.lei76@myhunter.cuny.edu>
2021-07-29 20:48:20 -0400

    ..

--- budget-planner/src/components/Navigation.js
+++ budget-planner/src/components/Navigation.js
@@ -47,8 +47,8 @@ function Navigation_bar(Pros){
           <div className="links">
           {!authState.status && (
             <>
-            <Link to="/users/login"> Login </Link>
-            <Link to="/users"> Register</Link>
+            <Link to="/users/login" className = "Navi_Items"> Login </Link>
+            <Link to="/users" className = "Navi_Items"> Register</Link>
             </>
           )}
            </div>
